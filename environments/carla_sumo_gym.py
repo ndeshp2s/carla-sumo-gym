ce17883a96b0f718e22ba6281ddfc68a199a203f
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-09-25 01:08:53 +0200

    - New functionality added. - To connect to carla server-client. - To start sumo simulator.

--- environments/carla_sumo_gym.py
+++ environments/carla_sumo_gym.py
@@ -3,6 +3,10 @@ import sys
 import subprocess
 import glob
 import time
+import json
+import re
+import random
+
 from os import path, environ
 import psutil
 import tempfile
@@ -11,7 +15,7 @@ import gym
 
 # carla library
 try:
-    sys.path.append(glob.glob('../../PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../carla/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
@@ -32,6 +36,7 @@ import traci
 
 from sumo_integration.carla_simulation import CarlaSimulation
 from sumo_integration.sumo_simulation import SumoSimulation
+from run_synchronization import SimulationSynchronization
 
 from util.netconvert_carla import netconvert_carla
 
@@ -39,41 +44,124 @@ from util.netconvert_carla import netconvert_carla
 class CarlaSumoGym(gym.Env):
     def __init__(self):
         super(CarlaSumoGym, self).__init__()
-        self.carla = CarlaSimulation('localhost', 2000, 0.1) # host, port, step_length
-        #self.sumo =  SumoSimulation(cfg_file, args.step_length, host=args.sumo_host, port=args.sumo_port, sumo_gui=args.sumo_gui, client_order=args.client_order)
+        self.client = None
+        self.world = None
+        self.synchronization = None
+        # # carla simulation
+        # self.carla_simulation = CarlaSimulation('localhost', 2000, 0.1) # host, port, step_length
+        # self.client = self.carla_simulation.client
+        # self.world = self.carla_simulation.client.get_world()
+        # current_map = self.world.get_map()
 
+        # # sumo simulation
+        # basedir = os.path.dirname(os.path.realpath(__file__))
+        # net_file = os.path.join(basedir, 'sumo_config', current_map.name + '.net.xml')
+        # cfg_file = os.path.join(basedir, 'sumo_config', current_map.name + '.sumocfg')
 
+        # self.sumo_net = sumolib.net.readNet(net_file)
+        # self.sumo_simulation =  SumoSimulation(cfg_file=cfg_file, step_length=0.1, host=None, port=None, sumo_gui=True, client_order=1)
 
+        # # synchronization
+        # self.synchronization = SimulationSynchronization(self.sumo_simulation, self.carla_simulation, 'none',
+        #                                         True, False)
 
+        # # Spawn the ego vehicle
+        # self.spawn_ego_vehicle()
 
+    def connect_server_client(self, display = True, rendering = True, synchronous = True, town = 'Town11', fps = 10.0):
+
+        self.kill_carla_server()
+
+        # open the server
+        p = None
+        cmd = [path.join(environ.get('CARLA_SERVER'), 'CarlaUE4.sh')]
+
+        if not display:
+            env_ =  {**os.environ, 'DISPLAY': ''}
+            cmd.append(" -opengl")
+            p = subprocess.Popen(cmd, env=env_)
+
+        else:
+            cmd.append(" -opengl")
+            p = subprocess.Popen(cmd)
 
         
-        # xodr_file = os.path.join(tmpdir + '/carla', current_map.name + '.xodr')
-        # # print(xodr_file)
-        # current_map.save_to_disk(xodr_file)
+        # connect to client
+        while True:
+            try:
+                carla_sim = CarlaSimulation('localhost', 2000, 10.0) # host, port, step_length
+                self.client = carla_sim.client
+                self.world = self.client.get_world()
 
-        # net_file = os.path.join(tmpdir + '/sumo', current_map.name + '.net.xml')
-        # print(net_file)
-        # netconvert_carla(xodr_file, net_file, guess_tls=True)
+                if self.world.get_map().name != town:
+                    carla.Client('localhost', 2000, 10).load_world(town)
+                    while True:
+                        try:
+                            while carla.Client('localhost', 2000, 10).get_world().get_map().name != town:
+                                time.sleep(0.1)
+                            break
+                        except:
+                            pass
+                break
+            except Exception as e:
+                time.sleep(0.1)
 
-        # basedir = os.path.dirname(os.path.realpath(__file__))
-        # print(basedir)
-        # vtypes_file = os.path.join(basedir, 'examples', 'carlavtypes.rou.xml')
+        # apply settings
+        delta_sec = 1.0 / fps
+        settings = self.world.get_settings()
+        self.world.apply_settings(carla.WorldSettings(no_rendering_mode = not rendering, synchronous_mode = synchronous, fixed_delta_seconds = delta_sec))
 
 
+        # open sumo simulator
+        current_map = self.world.get_map()
+        basedir = os.path.dirname(os.path.realpath(__file__))
+        net_file = os.path.join(basedir, 'sumo_config/net', current_map.name + '.net.xml')
+        cfg_file = os.path.join(basedir, 'sumo_config', current_map.name + '.sumocfg')
 
-def main():
-
-    test_carla_sumo_gym = CarlaSumoGym()
-    test_carla_sumo_gym.sumo_config_files()
-
+        sumo_net =  sumolib.net.readNet(net_file)
+        sumo_sim =  SumoSimulation(cfg_file=cfg_file, step_length=0.1, host=None, port=None, sumo_gui=True, client_order=1)
+        self.synchronization = SimulationSynchronization(sumo_sim, carla_sim, 'none', True, False)
 
 
-if __name__ == '__main__':
+    def tick(self):
+        self.synchronization.tick()
 
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print('\ndone.')
+
+    def spawn_ego_vehicle(self):
+        traci.vehicle.addFull('ego_vehicle', 'routeEgo', depart=None, departPos=str(20.0), departSpeed='0', typeID='vehicle.audi.etron')
+        traci.vehicle.setSpeedMode('ego_vehicle', int('00000',0))
+        traci.vehicle.setSpeed('ego_vehicle', 0.0)
+
+
+    def kill_carla_server(self):
+        binary = 'CarlaUE4.sh'
+        for process in psutil.process_iter():
+
+
+            if process.name().lower().startswith(binary.split('.')[0].lower()):
+                try:
+                    process.terminate()
+                except:
+                    pass
+
+        # Check if any are still alive, create a list
+        still_alive = []
+        for process in psutil.process_iter():
+            if process.name().lower().startswith(binary.split('.')[0].lower()):
+                still_alive.append(process)
+
+        # Kill process and wait until it's being killed
+        if len(still_alive):
+            for process in still_alive:
+                try:
+                    process.kill()
+                except:
+                    pass
+            psutil.wait_procs(still_alive)
+
+
+    def close(self):
+        if self.synchronization is not None:
+            self.synchronization.close()
+
+        self.kill_carla_server()
