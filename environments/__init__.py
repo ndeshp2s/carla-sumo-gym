7887af1e27934c5992f3ef24182136e91adf379e
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-09-28 08:26:04 +0200

    - Folder/file names changed.

--- environments/__init__.py
+++ environments/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='Urban-v0',
-    entry_point='environments.urban_env.urban_env_0:UrbanEnv0',
+    entry_point='environments.urban_env_0.env:UrbanEnv',
 )
