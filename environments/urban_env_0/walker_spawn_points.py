c55c7b42b66026050a45685241fa999b09525577
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-10-01 17:13:38 +0200

    - New file created for storing spawn points. - For Town11.

---
+++ environments/urban_env_0/walker_spawn_points.py
@@ -0,0 +1,133 @@
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('**/*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+
+point_spacing = 10
+
+walker_spawn_points = []
+
+for i in range(15, 90, point_spacing):
+    spawn_point = carla.Transform(carla.Location(x=i,y=-5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+    walker_spawn_points.append(spawn_point)
+
+    spawn_point = carla.Transform(carla.Location(x=i,y=5.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+    walker_spawn_points.append(spawn_point)
+
+spawn_point = carla.Transform(carla.Location(x=11,y=-5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=9,y=-5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+spawn_point = carla.Transform(carla.Location(x=11,y=5.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=9,y=5.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+spawn_point = carla.Transform(carla.Location(x=12,y=44.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=9,y=44.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+spawn_point = carla.Transform(carla.Location(x=12,y=55.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=9,y=55.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+
+for i in range(-185, -15, point_spacing):
+    spawn_point = carla.Transform(carla.Location(x=i,y=44.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+    walker_spawn_points.append(spawn_point)  
+
+    spawn_point = carla.Transform(carla.Location(x=i,y=55.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+    walker_spawn_points.append(spawn_point)  
+
+
+spawn_point = carla.Transform(carla.Location(x=-11,y=-5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-8,y=-5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+spawn_point = carla.Transform(carla.Location(x=-11,y=5.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-8,y=5.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+spawn_point = carla.Transform(carla.Location(x=-12,y=44.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-9,y=44.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+spawn_point = carla.Transform(carla.Location(x=-12,y=55.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-9,y=55.5,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+
+spawn_point = carla.Transform(carla.Location(x=5.5,y=61,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=61,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+
+
+for i in range(20, 43, point_spacing):
+
+  spawn_point = carla.Transform(carla.Location(x=5.5,y=i,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+  walker_spawn_points.append(spawn_point)  
+  spawn_point = carla.Transform(carla.Location(x=-5.5,y=i,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+  walker_spawn_points.append(spawn_point)  
+
+spawn_point = carla.Transform(carla.Location(x=5.5,y=9,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+spawn_point = carla.Transform(carla.Location(x=5.5,y=11,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=5.5,y=13,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+
+
+spawn_point = carla.Transform(carla.Location(x=5.5,y=38,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=5.5,y=37,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+
+
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=9,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=11,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=13,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+
+
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=40,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=38,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=37,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)     
+
+
+spawn_point = carla.Transform(carla.Location(x=5.5,y=-10,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=5.5,y=-12,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
+
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=-10,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point)
+spawn_point = carla.Transform(carla.Location(x=-5.5,y=-12,z=0.5), carla.Rotation(yaw=0, pitch=0, roll=0))
+walker_spawn_points.append(spawn_point) 
