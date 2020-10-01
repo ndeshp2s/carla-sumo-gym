fd58cfbd4697a5d27eb890475e15daf6d6c2f12a
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-10-01 17:10:21 +0200

    - New variables added in configuration.

--- environments/urban_env_0/config.py
+++ environments/urban_env_0/config.py
@@ -1,10 +1,21 @@
+# sumo related
+sumo_gui = False
 # ego vehicle start positon (in sumo)
-start_position = 20.0
+start_position = 150.0
 ev_type = 'vehicle.audi.etron'
 
 # carla simulation related 
-display = False
-rendering = False
+display = True
+rendering = True
 synchronous = True
 town = 'Town11'
 fps = 10.0
+
+# spawner related
+maximum_distance_pedestrian = 40.0
+minimum_distance_pedestrian = 0.0
+pedestrian_spawn_point_maximum = 30.0
+pedestrian_spawn_point_minimum = 8.0
+number_of_pedestrians = 10
+pedestrian_crossing_legal = 1.0
+pedestrian_crossing_illegal = 0.0
