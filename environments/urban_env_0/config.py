653b411cf572ec8b2e234bf95c5c7ff29ba113bb
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-10-06 08:49:44 +0200

    - New parameters added for rendering. - RGB sensor also added.

--- environments/urban_env_0/config.py
+++ environments/urban_env_0/config.py
@@ -1,17 +1,39 @@
-# sumo related
+### sumo related ###
+
 sumo_gui = False
+
 # ego vehicle start positon (in sumo)
 start_position = 150.0
 ev_type = 'vehicle.audi.etron'
 
-# carla simulation related 
+
+
+### carla simulation related ### 
+
 display = True
 rendering = True
 synchronous = True
 town = 'Town11'
 fps = 10.0
 
-# spawner related
+# sensor related
+rgb_sensor = True
+rgb_size_x = '1920'
+rgb_size_y = '1080'
+rgb_fov = '110'
+rgb_loc_x = -6.5
+rgb_loc_y = 0.0
+rgb_loc_z = 4.0
+
+# rendering related
+render = False
+screen_x = 720
+screen_y = 720
+
+
+
+### spawner related ###
+
 maximum_distance_pedestrian = 40.0
 minimum_distance_pedestrian = 0.0
 pedestrian_spawn_point_maximum = 30.0
