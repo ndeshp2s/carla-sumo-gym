8a679a62ab80ff2acc6bbb45ca30f8a6fe5caa14
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-10-19 11:47:29 +0200

    - Replay buffer created.

---
+++ agents/replay_buffer.py
@@ -0,0 +1,37 @@
+import random
+import numpy as np
+import torch
+from collections import deque, namedtuple
+
+
+class ReplayBuffer():
+    def __init__(self, buffer_size, buffer_size):
+
+        self.memory = deque(maxlen = buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names = ["state", "action", "reward", "next_state", "done"])
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def add_experience(self, state, action, reward, next_state, done):
+        experience = self.experience(state, action, reward, next_state, done)
+        self.memory.append(experience)
+
+    def add_episode(self, episode):
+        self.memory.append(episode)
+
+    def sample(self, batch_size = None):
+        if batch_size is None:
+            batch_size = self.batch_size
+
+        experiences = random.sample(self.memory, k = batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(self.device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(self.device)
+
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        return len(self.memory)
