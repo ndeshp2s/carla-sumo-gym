c2f370cf7ea67ed16400d6c73dfdf4cd34496cc4
ndeshp2s <niranjan.deshpande187@gmail.com>
2020-11-20 07:26:27 +0100

    - Code modified.

--- utils/renderer.py
+++ utils/renderer.py
@@ -14,49 +14,95 @@ class Renderer(object):
         self.pressed_keys = []
         self.is_open = False
 
+        self.rend_img = False
+        self.rent_q_val = False
+        self.rend_grid = False
+        self.img_win_size = (0, 0)
+        self.q_values_win_size = (0, 0)
+        self.grid_window_size = (0, 0)
+        self.grid_size = (0, 0)
+        self.grid_resolution = 0.0
 
-    def create_screen(self, width, height):
+        self.GREEN = (0, 153, 76)
+        self.ORANGE = (204, 102, 0)
+        self.RED = (204, 0, 0)
+        self.YELLOW = (204, 204, 0)
+        self.BLACK = (0, 0, 0)
+        self.OFF_WHITE = (200, 200, 200)
+        self.WHITE = (255, 255, 255)
+
+        pygame.font.init()
+
+
+
+
+    def create_screen(self, width = 720, height = 720, render_image = True, render_q_values = True, render_grid = True, grid_x = 0, grid_y = 0, grid_resolution = 0.5):
         """
         Creates a pygame window
         :param width: the width of the window
         :param height: the height of the window
         :return: None
         """
-        self.size = (width, height + 140)
+        if render_image:
+            self.img_win_size = (720, 720)
+            self.rend_img = True
+        if render_q_values:
+            self.q_values_win_size = (720, 100)
+            self.rent_q_val = True
+        if render_grid: 
+            self.grid_window_size = (640, 940) 
+            self.rend_grid = True
+            self.grid_size = (grid_x, grid_y)
+            self.grid_resolution = grid_resolution
+        
+        self.border_margin = 20 
+        x = self.border_margin + self.img_win_size[0] + self.border_margin + self.grid_window_size[0] + self.border_margin
+        y = self.border_margin + self.grid_window_size[1] + self.border_margin
+
+        self.size = (x, y)
         self.screen = self.display.set_mode(self.size, HWSURFACE | DOUBLEBUF)
         self.display.set_caption("Renderer")
         self.is_open = True
 
 
-    def render_image(self, image, image_frame = 0, model_output = None, speed = 0):
+    def render(self, image, q_values = None, grid = None):
         """
         Render the given image to the pygame window
         :param image: a grayscale or color image in an arbitrary size. assumes that the channels are the last axis
         :return: None
         """
         if self.is_open:
-            if len(image.shape) == 2:
-                image = np.stack([image] * 3)
-            if len(image.shape) == 3:
-                if image.shape[0] == 3 or image.shape[0] == 1:
-                    image = np.transpose(image, (1, 2, 0))
-            surface = pygame.surfarray.make_surface(image.swapaxes(0, 1))
-            surface = pygame.transform.scale(surface, (720, 720))
+            self.screen.fill(self.OFF_WHITE)
 
-            if model_output is not None:
-                self.show(q = model_output, speed = speed)
-            bright = pygame.Surface((surface.get_width(), surface.get_height()), flags=pygame.SRCALPHA)
-            bright.fill((40, 40, 40, 0))
-            surface.blit(bright, (0, 140), special_flags=pygame.BLEND_RGBA_ADD)
+            if self.render_image:
+                self.render_image(image = image)
+            if self.render_q_values:
+                self.render_q_values(q_values = q_values)
+            if self.rend_grid:
+                self.render_grid(grid = grid)
 
-            
-
-            self.screen.blit(surface, (0, 140))
             self.display.flip()
             self.clock.tick()
             self.get_events()
 
-            self.save_image(image = self.screen, frame = image_frame)
+            #self.save_image(image = self.screen, frame = image_frame)
+
+
+    def render_image(self, image):
+        """
+        Render the given image to the pygame window
+        :param image: a grayscale or color image in an arbitrary size. assumes that the channels are the last axis
+        :return: None
+        """
+        if len(image.shape) == 2:
+            image = np.stack([image] * 3)
+        if len(image.shape) == 3:
+            if image.shape[0] == 3 or image.shape[0] == 1:
+                image = np.transpose(image, (1, 2, 0))
+        surface = pygame.surfarray.make_surface(image.swapaxes(0, 1))
+        surface = pygame.transform.scale(surface, (720, 720))
+        
+        self.screen.blit(surface, (self.border_margin, (self.size[1] - self.img_win_size[1] - self.border_margin)))
 
 
     def save_image(self, image, frame):
@@ -82,13 +128,47 @@ class Renderer(object):
             elif event.type == pygame.QUIT:
                 self.close()
 
+
+    def render_q_values(self, q_values):
+
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        font_file = os.path.join(base_dir, 'Roboto-Light.ttf')
+        font = pygame.font.Font(font_file, 18)
+
+        normalized_q_values = self.normalize_array(q_values)
+        surface = pygame.Surface((self.q_values_win_size[0],  self.q_values_win_size[1] + self.border_margin))
+        surface.fill(self.OFF_WHITE)
+        surface.set_alpha(150)
+
+        # 0th action
+        pygame.draw.rect(surface, self.GREEN, (0, 100, 180, -100))
+        text = font.render("Accelerate", True, (0, 0, 0))
+        surface.blit(text, (1, self.q_values_win_size[1]))
+
+        # 1th action
+        pygame.draw.rect(surface, self.ORANGE, (180, 100, 180, -100))
+        text = font.render("Decelerate", True, (0, 0, 0))
+        surface.blit(text, (181, self.q_values_win_size[1]))
+
+        # 2th action
+        pygame.draw.rect(surface, self.YELLOW, (360, 100, 180, -100))
+        text = font.render("Steer", True, (0, 0, 0))
+        surface.blit(text, (361, self.q_values_win_size[1]))
+
+        # 3th action
+        pygame.draw.rect(surface, self.RED, (540, 100, 180, -100))
+        text = font.render("Brake", True, (0, 0, 0))
+        surface.blit(text, (541, self.q_values_win_size[1]))
+
+        pygame.draw.rect(surface, self.BLACK, pygame.Rect(0, 0, self.q_values_win_size[0], self.q_values_win_size[1] + 20), 1)
+
+        self.screen.blit(surface, (self.border_margin, (self.size[1] - self.img_win_size[1] - 2*self.border_margin - self.q_values_win_size[1] - self.border_margin)))
+
+
     def show(self, q, speed = 0):
         pygame.font.init()
-        self.GREEN = (0, 153, 76)
-        self.ORANGE = (204, 102, 0)
-        self.RED = (204, 0, 0)
-        self.YELLOW = (204, 204, 0)
-        self.screen.fill((200, 200, 200))
+
+        self.screen.fill(self.OFF_WHITE)
 
         base_dir = os.path.dirname(os.path.realpath(__file__))
         font_file = os.path.join(base_dir, 'Roboto-Light.ttf')
@@ -188,3 +268,76 @@ class Renderer(object):
         """
         self.is_open = False
         pygame.quit()
+
+
+    def render_grid(self, grid):
+        WIDTH = 20
+        HEIGHT = 20
+        MARGIN = 5
+        
+        font = pygame.font.SysFont('arial', 12, False)
+        surface = pygame.Surface((self.grid_window_size[0], self.grid_window_size[1]))
+        surface.fill(self.WHITE)
+        BLACK = (0, 0, 0)
+        for row in range(30):
+            row_cord = MARGIN + row * HEIGHT
+
+            if row < 10:
+                indent = '   '
+            else:
+                indent = '  '
+            text = font.render(indent + str(row - 4), 1, (0, 0, 0))
+            surface.blit(pygame.transform.flip(text, False, True), (5 + 20 * WIDTH, row_cord))
+
+            for column in range(20):
+                col_cord = MARGIN + column * WIDTH
+
+                if column < 10:
+                    indent = '  '
+                else:
+                    indent = ' '
+                text = font.render(indent + str(column - 10), 1, (0, 0, 0))
+                surface.blit(pygame.transform.flip(text, False, True), (col_cord, 5 + 30 * HEIGHT))
+
+                color = self.BLACK
+
+                if grid[row][column] == 0.33:
+                    #color = (0, 255, 0)
+                    pygame.draw.rect(surface, self.RED, [(WIDTH) * (column) + MARGIN, (HEIGHT) * row + MARGIN, (WIDTH), (HEIGHT)])
+
+                elif grid[row][column] == 0.67:
+                    #color = (0, 255, 0)
+                    pygame.draw.rect(surface, self.GREEN, [(WIDTH) * (column) + MARGIN, (HEIGHT) * row + MARGIN, (WIDTH), (HEIGHT)])
+
+                elif grid[row][column] == 1.0:
+                    #color = (0, 255, 0)
+                    pygame.draw.rect(surface, self.ORANGE, [(WIDTH) * (column) + MARGIN, (HEIGHT) * row + MARGIN, (WIDTH), (HEIGHT)])
+
+                pygame.draw.rect(surface, color, pygame.Rect(col_cord, row_cord, HEIGHT, WIDTH), 1)
+
+               
+        #self.screen.blit(pygame.transform.rotate(surface, 180), (740, 0))
+        self.screen.blit(pygame.transform.flip(surface, False, True), ((self.border_margin + self.img_win_size[0] + self.border_margin), (self.size[1] -  self.grid_window_size[1] - self.border_margin)))
+
+        # WIDTH = 20
+        # HEIGHT = 20
+ 
+        # # This sets the margin between each cell
+        # MARGIN = 5
+        # surface = pygame.Surface((640, 940))
+        # surface.fill((255, 255, 255))
+        # for row in range(45):
+        #     for column in range(30):
+        #         color = (0, 0, 0)
+        #         # if grid[row][column] == 1:
+        #         #     color = GREEN
+        #         pygame.draw.rect(surface,
+        #                          color,
+        #                          [(MARGIN + WIDTH) * (column) + MARGIN,
+        #                           (MARGIN + HEIGHT) * row + MARGIN,
+        #                           WIDTH,
+        #                           HEIGHT])
+        # #         # font = pygame.font.SysFont('1,2', 5)
+        # #         # screen.blit(font.render('Hello!', True, (255,0,0)), (200, 100))
+
+        # self.screen.blit(surface, (0, 0))
